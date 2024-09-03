32fa9fea232f8ac19b876b1f7a792198ce52e38a
queenJiJi <jiyeon.park@mail.mcgill.ca>
2024-09-03 17:00:30 +0900

    배포용 url 변경

--- api/base.js
+++ api/base.js
@@ -3,7 +3,9 @@ import { getAuthToken } from '@/lib/utils/user';
 import axios from 'axios'; // axios: 백엔드에 요청보낼때 쓰는 것
 
 const instance = axios.create({ // axios 객체로서 baseURL 도메인으로 요청을 보내는 것
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',// 로컬용
+    baseURL: 'https://studydate-next-sigma.vercel.app',// 배포
+
     headers:{
         'Content-Type': 'application/json', // 요청을 날릴때 서버한테 응답데이터를 '어떤 구조의 응답을 줘'라고 명시하는 것
     }
