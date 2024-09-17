f22472633e6a025c69934a1fbfd9f1af013ccc14
queenJiJi <jiyeon.park@mail.mcgill.ca>
2024-09-17 23:25:42 +0900

    .

--- components/profile/molecules/ProfileImage.js
+++ components/profile/molecules/ProfileImage.js
@@ -15,6 +15,7 @@ const ProfileImage = ({src, alt}) => {
                 height={160} // 이미지 높이 설정
                 // layout="fixed"  // 레이아웃 유형 설정
                 // objectFit="cover" // 이미지 비율 유지
+                // priority
                 style={{
                     borderRadius: '50%', // CSS 스타일 직접 적용
                     boxShadow: 'rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
